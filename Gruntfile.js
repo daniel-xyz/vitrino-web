f76b148e2cc616af520f732026cb29161da66665
Daniel Bischoff <danielbischoff@hotmail.com>
2016-09-21 22:02:12 +0200

    setup deployment via Travis CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,8 +63,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('test', ['mochaTest']);
-  grunt.registerTask('lint', ['eslint']);
   grunt.registerTask('build', ['less', 'cssmin']);
-  grunt.registerTask('default', ['build', 'lint', 'watch']);
+  grunt.registerTask('test', ['eslint', 'mochaTest']);
+  grunt.registerTask('default', ['build', 'test', 'watch']);
 };
