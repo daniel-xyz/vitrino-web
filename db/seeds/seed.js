156dbf4cfc05948bc85f8225cf77b921a6f36126
Daniel Bischoff <danielbischoff@hotmail.com>
2016-11-15 16:39:08 +0100

    replaced hardcoded column id's in seed-file with dynamic statements

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -13,15 +13,12 @@ function roles () {
     .then(function () {
       return Promise.all([
         knex('roles').insert({
-          id: 1,
           name: 'user'
         }),
         knex('roles').insert({
-          id: 2,
           name: 'vendor'
         }),
         knex('roles').insert({
-          id: 3,
           name: 'admin'
         })
       ]);
@@ -33,35 +30,27 @@ function productCategories () {
     .then(function () {
       return Promise.all([
         knex('product_categories').insert({
-          id: 1,
           name: 'Mode & Accessoires'
         }),
         knex('product_categories').insert({
-          id: 2,
           name: 'Schmuck'
         }),
         knex('product_categories').insert({
-          id: 3,
           name: 'Design & Geschenkartikel'
         }),
         knex('product_categories').insert({
-          id: 4,
           name: 'Parfumerie & Kosmetik'
         }),
         knex('product_categories').insert({
-          id: 5,
           name: 'Kunst'
         }),
         knex('product_categories').insert({
-          id: 6,
           name: 'Hobby'
         }),
         knex('product_categories').insert({
-          id: 7,
           name: 'Haus & Wohnen'
         }),
         knex('product_categories').insert({
-          id: 8,
           name: 'Kinder'
         })
       ])
@@ -73,7 +62,6 @@ function addresses () {
     .then(function () {
       return Promise.all([
         knex('addresses').insert({
-          id: 1,
           street: 'Pfuelstraße 5',
           city: 'Berlin',
           zip_code: 10997
@@ -87,31 +75,28 @@ function users () {
     .then(function () {
       return Promise.all([
         knex('users').insert({
-          id: 1,
           email: email_dummy_user,
           username: 'Dummy User',
           password: authHelper.generateSecureHash(password),
           email_verified: true,
           auth_token: authHelper.generateToken(email_dummy_user),
-          role_id: 1
+          role_id: knex.raw("(SELECT id FROM roles WHERE name='user')")
         }),
         knex('users').insert({
-          id: 2,
           email: email_dummy_vendor,
           username: 'Dummy Vendor',
           password: authHelper.generateSecureHash(password),
           email_verified: true,
           auth_token: authHelper.generateToken(email_dummy_vendor),
-          role_id: 2
+          role_id: knex.raw("(SELECT id FROM roles WHERE name='vendor')")
         }),
         knex('users').insert({
-          id: 3,
           email: email_dummy_admin,
           username: 'Dummy Admin',
           password: authHelper.generateSecureHash(password),
           email_verified: true,
           auth_token: authHelper.generateToken(email_dummy_admin),
-          role_id: 3
+          role_id: knex.raw("(SELECT id FROM roles WHERE name='admin')")
         })
       ]);
     });
@@ -122,24 +107,21 @@ function companies () {
     .then(function () {
       return Promise.all([
         knex('companies').insert({
-          id: 1,
           name: 'Schuhzauberei GmbH',
           company_verified: true,
-          user_id: 2,
+          user_id: knex.raw("(SELECT id FROM users WHERE email='" + email_dummy_vendor + "')"),
           address_id: 1
         }),
         knex('companies').insert({
-          id: 2,
           name: 'Die Glaserei GmbH & Co. KG',
           company_verified: false,
-          user_id: 3,
+          user_id: knex.raw("(SELECT id FROM users WHERE email='" + email_dummy_admin + "')"),
           address_id: 1
         }),
         knex('companies').insert({
-          id: 3,
           name: 'We will hurt you AG',
           company_verified: false,
-          user_id: 3,
+          user_id: knex.raw("(SELECT id FROM users WHERE email='" + email_dummy_admin + "')"),
           address_id: 1
         })
       ]);
@@ -151,44 +133,39 @@ function products () {
     .then(function () {
       return Promise.all([
         knex('products').insert({
-          id: 1,
           name: 'Winterschuhe',
           description: 'Ganz tolle Schuhe.',
           image_url: '',
           product_category_id: 1,
-          company_id: 1
+          company_id: knex.raw("(SELECT id FROM companies WHERE name='Schuhzauberei GmbH')")
         }),
         knex('products').insert({
-          id: 2,
           name: 'Goldene Sneaker',
           description: 'Bitte Gewicht beachten!',
           image_url: '',
           product_category_id: 1,
-          company_id: 1
+          company_id: knex.raw("(SELECT id FROM companies WHERE name='Schuhzauberei GmbH')")
         }),
         knex('products').insert({
-          id: 3,
           name: 'Eine wundersamer Schuhkarton',
           description: 'Was mag wohl drin sein?!',
           image_url: '',
           product_category_id: 1,
-          company_id: 1
+          company_id: knex.raw("(SELECT id FROM companies WHERE name='Schuhzauberei GmbH')")
         }),
         knex('products').insert({
-          id: 4,
           name: 'Grüne Tanzschuhe',
           description: 'Ganz tolle Schuhe.',
           image_url: '',
           product_category_id: 1,
-          company_id: 1
+          company_id: knex.raw("(SELECT id FROM companies WHERE name='Schuhzauberei GmbH')")
         }),
         knex('products').insert({
-          id: 5,
           name: 'Jack Wolfskin Sandalen',
           description: 'Für die nächste Weltreise.',
           image_url: '',
           product_category_id: 1,
-          company_id: 1
+          company_id: knex.raw("(SELECT id FROM companies WHERE name='Schuhzauberei GmbH')")
         })
       ]);
     });
@@ -199,10 +176,9 @@ function stores () {
     .then(function () {
       return Promise.all([
         knex('stores').insert({
-          id: 1,
           name: 'Laden in Friedrichshain',
-          company_id: 1,
-          user_id: 2,
+          company_id: knex.raw("(SELECT id FROM companies WHERE name='Schuhzauberei GmbH')"),
+          user_id: knex.raw("(SELECT id FROM users WHERE email='" + email_dummy_vendor + "')"),
           address_id: 1
         })
       ])
